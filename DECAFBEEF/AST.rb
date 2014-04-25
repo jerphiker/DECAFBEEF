413d502bf94f3ff4ceca7dca1cf3944e4d00a9c6
kmacoskey <kmacoske@mymail.mines.edu>
2014-04-25 07:06:05 -0600

    combined everything

--- DECAFBEEF/AST.rb
+++ DECAFBEEF/AST.rb
@@ -1,17 +1,26 @@
 require 'securerandom'
 
-class AbsNode
-  attr_accessor :list, :desc, :unique_id
+module Visitable
+  def accept visitor
+    visitor.visit(self)
+  end
+end
 
-  def initialize(desc, *b )
+class AbsNode
+  attr_accessor :list, :name, :unique_id, :attrib
+
+  include Visitable
+
+  def initialize(desc, a, *b )
     @list = b
-    @desc = desc
+    @name = desc
     @unique_id = SecureRandom.uuid
+    @attrib = a
   end
 
   def get_ids(i = 0)
     #ret = "#{' ' * i}#{self.object_id} #{@desc}\n"
-    ret = "#{@unique_id} #{@desc}\n"
+    ret = "#{@unique_id} #{@name}\n"
     @list.each do |child|
       if child == nil
         next
@@ -65,4 +74,41 @@ class Expr < AbsNode
 end
 
 class IfNode < AbsNode
+end
+
+class Ast < AbsNode
+  attr_accessor :root
+
+  def initialize(r)
+    @root = r
+  end
+
+  def accept visitor
+    @root.accept visitor
+    @root.list.each do |node|
+      node.accept visitor 
+      iterate(node, visitor)
+    end
+  end
+
+  def iterate(node, visitor)
+    node.list.each do |next_node|
+      if next_node.respond_to?(:accept)
+        next_node.accept visitor
+        iterate(next_node, visitor)
+      else
+       next_node = AbsNode.new(next_node, nil)
+       next_node.accept visitor
+      end
+    end
+
+
+  end
+end
+
+class BaseVisitor
+  def visit subject
+    method_name = "visit_#{subject.class}".intern
+    send(method_name, subject )
+  end
 end
