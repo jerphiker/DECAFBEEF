76ce6af86aff2a337166680e48b2aa44b4d4dffd
kmacoskey <kmacoske@mymail.mines.edu>
2014-05-02 09:20:09 -0600

    lots

--- DECAFBEEF/AST.rb
+++ DECAFBEEF/AST.rb
@@ -1,15 +1,11 @@
 require 'securerandom'
 
-module Visitable
-  def accept(visitor, outa, outp, outir)
-    visitor.visit(self, outa, outp, outir)
-  end
-end
-
 class AbsNode
-  attr_accessor :list, :name, :unique_id, :attrib, :ir
+  attr_accessor :list, :name, :unique_id, :attrib, :ir, :parent, :leftmost_sib, :right_sib
 
-  include Visitable
+  def accept(visitor)
+    visitor.visit(self)
+  end
 
   def initialize(desc, a, *b )
     @list = b
@@ -17,8 +13,43 @@ class AbsNode
     @unique_id = SecureRandom.uuid
     @attrib = a
     @ir = String.new
+
+    @parent = nil
+    @leftmost_sib = nil
+    @right_sib = nil
+
   end
 
+end
+
+class Ast < AbsNode
+  attr_accessor :root
+
+  def initialize(r)
+    @root = r
+  end
+
+  def accept(visitor)
+    @root.accept(visitor)
+    @root.list.each do |node|
+      if node != nil
+        node.accept(visitor)
+        iterate(node, visitor)
+      end
+    end
+  end
+
+  def iterate(node, visitor)
+    node.list.each do |next_node|
+      if next_node.respond_to?(:accept)
+        next_node.accept(visitor)
+        iterate(next_node, visitor)
+      else
+
+      end
+    end
+
+  end
 end
 
 class Root < AbsNode
@@ -36,7 +67,7 @@ end
 class Dec < AbsNode
 end
 
-class DirectDec < AbsNode
+class GlobalDec < AbsNode
 end
 
 class RExpr < AbsNode
@@ -63,69 +94,7 @@ end
 class Literal < AbsNode
 end
 
-class Ast < AbsNode
-  attr_accessor :root
-
-  def initialize(r)
-    @root = r
-  end
-
-  def accept(visitor, outa, outp, outir)
-    @root.accept(visitor, outa, outp, outir)
-    @root.list.each do |node|
-      if node != nil
-        node.accept(visitor, outa, outp, outir)
-        iterate(node, visitor, outa, outp, outir)
-      end
-    end
-  end
-
-  def iterate(node, visitor, outa, outp, outir)
-    node.list.each do |next_node|
-      if next_node.respond_to?(:accept)
-        next_node.accept(visitor, outa, outp, outir)
-        iterate(next_node, visitor, outa, outp, outir)
-      else
-       # next_node = AbsNode.new(next_node, nil)
-       # next_node.accept visitor
-      end
-    end
 
 
-  end
-end
 
-class AstVisitorPass1 
 
-  def visit(subject, outa, outp, outir)
-    #if subject.name == 'Lambda' then return end
-    #puts "#{subject.unique_id} #{subject.name} :: #{subject.attrib} :: #{subject.class} :: #{subject.ir}"
-    outa << "#{subject.unique_id} #{subject.attrib}\n"
-    outp <<  "#{subject.unique_id} #{subject.attrib}\n"
-  end
-
-end
-
-class AstVisitorPass2
-
-  def visit(subject, outa, outp, outir)
-    #if subject.name == 'Lambda' then return end
-    ret = "#{subject.unique_id}" 
-    subject.list.each do |child|
-      if child != nil
-        ret += " #{child.unique_id}"
-      end
-    end
-    #puts ret
-    outa << ret << "\n"
-    outp << ret << "\n"
-  end
-
-end
-
-class BaseVisitor
-  def visit subject, outa, outp, outir
-    method_name = "visit_#{subject.class}".intern
-    send(method_name, subject)
-  end
-end
